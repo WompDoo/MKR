b9c72c3a8741fcd0d50aa48b59de4fa53a3d4126
Rete-Keyt Uibo <Rete-Keyt.Uibo@reach-u.com>
2017-01-10 16:41:47 +0200

    Fixed the mobile navigation, added it to other pages

--- js/main.js
+++ js/main.js
@@ -1,3 +1,30 @@
-/**
- * Created by DataKeyt on 21.12.2016.
- */
+$(function () {
+    var i = 0;
+    $('#mobileMenuLink').on('click', function () {
+        i++;
+        if (i % 2 != 0) {
+            $('#mobileNav').css({
+                'max-height': '999px',
+                'transition-property': 'all',
+                'transition-duration': '.5s',
+                'transition-timing-function': 'cubic-bezier(0, 1, 0.5, 1)',
+                'overflow': 'visible',
+                'visibility': 'visible'
+
+            });
+        } else {
+            $('#mobileNav').css({
+                'max-height': '0',
+                'overflow': 'hidden',
+                'visibility': 'hidden'
+            });
+        }
+    });
+});
+
+//Prevent the function to run before the document is loaded
+document.addEventListener('readystatechange', function () {
+    if (document.readyState === "complete") {
+
+    }
+});
