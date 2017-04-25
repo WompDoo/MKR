db7420250284bc163a5c998aeba18ec4085ebad5
Rete-Keyt Uibo <Rete-Keyt.Uibo@reach-u.com>
2017-04-25 10:43:03 +0300

    renamed furniture.php controller to products.php

--- furniture.php
+++ furniture.php
@@ -1,6 +1,6 @@
 <?php
 include('header.php');
-include('controllers/furniture.php');
+include('controllers/products.php');
 ?>
 
 <!DOCTYPE html>
--- sketchbooks.php
+++ sketchbooks.php
@@ -1,5 +1,5 @@
 <?php include('header.php');
-include('./controllers/furniture.php');
+include('./controllers/products.php');
 ?>
 
 <!DOCTYPE html>
